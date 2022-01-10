1fd821419544067c348f515a5f145c95234e4410
mridul <mridul127cb@gmail.com>
2022-01-10 16:58:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index1.html");
+});
+app.listen(process.env.PORT||3000,function(){
+  console.log("server is running on 3000");
+});
