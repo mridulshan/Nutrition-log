ffd22fb7fca42c87999975c9dd5391760e0660d8
mridul <mridul127cb@gmail.com>
2022-01-15 18:58:45 +0530

    with ejs v1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs =require("ejs");
 
 const app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 app.get("/",function(req,res){
-  res.sendFile(__dirname + "/index1.html");
+  res.render("home");
 });
+
+app.get("/calories",function(req,res){
+  res.render("calories");
+});
+app.get("/exercise",function(req,res){
+  res.render("exercise");
+});
+
 app.listen(process.env.PORT||3000,function(){
   console.log("server is running on 3000");
 });
