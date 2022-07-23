2cef62e544ad025fc4d2183f1e046dfd5dc3f55a
mridul <mridul127cb@gmail.com>
2022-07-24 00:04:28 +0530

    login and mongoose added

--- app.js
+++ app.js
@@ -6,44 +6,43 @@ const ejs = require("ejs");
 const https = require('https');
 var arrFromSearch=1;
 var arrays=[];
-var obj={
-  total_hits: 0,
-  max_score: 0.00,
-  hits: [
-    {
-      _index: 0,
-      _type: 'nil',
-      _id: 'none',
-      _score: 0,
-      fields:{item_name:'enter item ..!.'}
-    },
-    {
-      _index: 0,
-      _type: 'nil',
-      _id: 'none',
-      _score: 0,
-      fields:{item_name:'enter item ..!.'}
-    },
-    {
-      _index: 0,
-      _type: 'nil',
-      _id: 'none',
-      _score: 0,
-      fields:{item_name:'enter item ..!.'}
-    },
-    {
-      _index: 0,
-      _type: 'nil',
-      _id: 'none',
-      _score: 0,
-      fields:{item_name:'enter item ..!.'}
-    },
-  ]};
-// const nutritionix = require("nutritionix-api");
 
-// const YOUR_APP_ID = "dc210fd5"; // Your APP ID
-// const YOUR_API_KEY = "135a6af303c7c5cde8026f549f61e283"; // Your KEY
-// nutritionix.init(YOUR_APP_ID, YOUR_API_KEY); 
+//mongodb update
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/peopleDb');
+const userSchema = new mongoose.Schema({
+  name: {
+      type:String,
+      required: [true,"Name is required"]
+  },
+  age: Number,
+  userName: {
+      type:String,
+      required: true
+  },
+  password: {
+      type:String,
+      required: true
+  }
+});
+const Person = mongoose.model("Person",userSchema);
+
+// const nutritionSchema = new mongoose.Schema({
+//    user: Person,
+//    food:{
+//     type:String,
+//     required:true,
+//    },
+//    serving:{
+//     type:Number,
+//     required:true,
+//    },
+//    calorie:{
+//     type:Number,
+//     required:true,
+//    }
+// });
+
 
 const app = express();
 app.set("view engine", "ejs");
@@ -56,42 +55,84 @@ app.get("/", function (req, res) {
 
 app.get("/calories", function (req, res) {
   let arrayLen =arrays.length;
-
-  if(obj==0){
     res.render("calories",{recievedFromApp:arrays,len:arrayLen});
-  }else{
-    res.render("calories",{recievedFromApp:arrays,len:arrayLen,foodFromApi:obj});
-    }
-  
+    console.log();
 });
-app.post("/calories", function (req, res) {
-  var arrayLen =arrays.length;
 
+app.post("/calories", function (req, res){
+  var arrayLen =arrays.length;
   arrFromSearch=req.body;
   if(req.body.postFood!==''){
   arrays.push(arrFromSearch);
   };
   // console.log(arrays.length);
-  const url ="https://api.nutritionix.com/v1_1/search/"+ req.body.postFood+"?results=0:20&fields=item_name,nf_total_fat,brand_name,item_id,nf_calories,nf_protein&appId=dc210fd5&appKey=135a6af303c7c5cde8026f549f61e283#";
-    https.get(url, function (response) {
-    console.log("Response status code="+response.statusCode);
-    response.on("data", function (data) {
-       obj = JSON.parse(data);
-      // for (let i = 0; i < obj.hits.length; i++) {
-      //   console.log(obj.hits[i].fields.item_name,obj.hits[i].fields.nf_serving_size_qty,obj.hits[i].fields.nf_protein,obj.hits[i].fields.nf_calories);
-      // }
-      // console.log(obj.hits[i].fields.nf_serving_size_qty);
-    });
-  });
-  console.log(arrays);
+
+  //comment here
+  // const url ="https://api.nutritionix.com/v1_1/search/"+ req.body.postFood+"?results=0:20&fields=item_name,nf_total_fat,brand_name,item_id,nf_calories,nf_protein&appId=dc210fd5&appKey=135a6af303c7c5cde8026f549f61e283#";
+  //   https.get(url, function (response) {
+  //   console.log("Response status code="+response.statusCode);
+  //   response.on("data", function (data) {
+  //      obj = JSON.parse(data);
+  //     // for (let i = 0; i < obj.hits.length; i++) {
+  //     //   console.log(obj.hits[i].fields.item_name,obj.hits[i].fields.nf_serving_size_qty,obj.hits[i].fields.nf_protein,obj.hits[i].fields.nf_calories);
+  //     // }
+  //     // console.log(obj.hits[i].fields.nf_serving_size_qty);
+  //   });
+  // });
     res.redirect("/calories#previewLog");
-    
-    
 });
+
 
 app.get("/exercise", function (req, res) {
   res.render("exercise");
 });
+
+app.get("/login",function(req,res){
+  
+
+  res.render("login");
+});
+
+app.post("/login",function(req,res){
+  const testName=req.body.uName;
+  const testPass=req.body.uPass;
+  Person.find({username:testName, password:testPass},function(err,persons){
+      if(err)
+      console.log(err);
+      else{
+        if(persons.length>0){
+            console.log("This will be taken to new Page!")
+        }else{
+          console.log("No user found");
+        }
+      }
+  });
+ 
+  res.redirect("/login");
+});
+
+app.post("/signup",function(req,res){  
+  const person = new Person({
+      name: req.body.uFName,
+      age: req.body.uAge,
+      userName: req.body.uName,
+      password: req.body.uPass,
+  });
+  person.save();
+
+  //to see database of users 
+
+  // Person.find(function(err,persons){
+  //   if(err)
+  //        console.log(err);
+  //   else
+  //       persons.forEach(element => {
+  //           console.log(element.name);
+  //       });
+  // });
+  res.redirect("/");
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server is running on 3000");
