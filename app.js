e1a1917ad73d3beb411364f2bd4c6f7ae7883d18
mridul <mridul127cb@gmail.com>
2022-07-29 10:44:40 +0530

    Api Search

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const https = require('https');
 var arrFromSearch;
 var arrays=[];
+var storeApi=[];
 var pArray=[];
 var mainProfile={
 
@@ -67,7 +68,10 @@ app.get("/", function (req, res) {
 
 app.get("/calories", function (req, res) {
   let arrayLen =arrays.length;
-    res.render("calories",{recievedFromApp:arrays,len:arrayLen});
+    
+      res.render("calories",{recievedFromApp:arrays,len:arrayLen, rStoreApi:storeApi });
+    
+    storeApi=[];
     
 });
 
@@ -79,18 +83,7 @@ app.post("/calories", function (req, res){
   };
   // console.log(arrays.length);
 
-  //comment here
-  // const url ="https://api.nutritionix.com/v1_1/search/"+ req.body.postFood+"?results=0:20&fields=item_name,nf_total_fat,brand_name,item_id,nf_calories,nf_protein&appId=dc210fd5&appKey=135a6af303c7c5cde8026f549f61e283#";
-  //   https.get(url, function (response) {
-  //   console.log("Response status code="+response.statusCode);
-  //   response.on("data", function (data) {
-  //      obj = JSON.parse(data);
-  //     // for (let i = 0; i < obj.hits.length; i++) {
-  //     //   console.log(obj.hits[i].fields.item_name,obj.hits[i].fields.nf_serving_size_qty,obj.hits[i].fields.nf_protein,obj.hits[i].fields.nf_calories);
-  //     // }
-  //     // console.log(obj.hits[i].fields.nf_serving_size_qty);
-  //   });
-  // });
+ 
   if(req.body.svg){
     for(var i=req.body.svg-1;i<arrayLen-1;i++){
       arrays[i]=arrays[i+1];
@@ -219,6 +212,23 @@ app.post("/signup",function(req,res){
   res.redirect("/login");
 });
 
+app.post("/apicall",function(req,res){
+
+  const url ="https://api.nutritionix.com/v1_1/search/"+req.body.postSearch+"?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=dc210fd5&appKey=0fc21a0f999dee329579e3854194acd5";
+    https.get(url, function (response) {
+    console.log("Response status code="+response.statusCode);
+    response.on("data", function (data) {
+       obj = JSON.parse(data);
+      
+       obj.hits.forEach(element => {
+        storeApi.push(element.fields);
+       });
+       console.log(storeApi);
+       res.redirect("/calories/#openIt");
+    });
+  });
+});
+
 
 
 
@@ -238,4 +248,18 @@ app.listen(process.env.PORT || 3000, function () {
   //       persons.forEach(element => {
   //           console.log(element.name);
   //       });
+  // });
+
+
+   //comment here
+  // const url ="https://api.nutritionix.com/v1_1/search/"+ req.body.postFood+"?results=0:20&fields=item_name,nf_total_fat,brand_name,item_id,nf_calories,nf_protein&appId=dc210fd5&appKey=135a6af303c7c5cde8026f549f61e283#";
+  //   https.get(url, function (response) {
+  //   console.log("Response status code="+response.statusCode);
+  //   response.on("data", function (data) {
+  //      obj = JSON.parse(data);
+  //     // for (let i = 0; i < obj.hits.length; i++) {
+  //     //   console.log(obj.hits[i].fields.item_name,obj.hits[i].fields.nf_serving_size_qty,obj.hits[i].fields.nf_protein,obj.hits[i].fields.nf_calories);
+  //     // }
+  //     // console.log(obj.hits[i].fields.nf_serving_size_qty);
+  //   });
   // });
